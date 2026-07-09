60e3add8edf27e016d135fab6f62b3adacb41db6
Sampannasapkota <henryatokpas12@gmail.com>
2026-07-09 14:29:45 +0545

    Initial Spring Boot project setup

---
+++ src/main/java/com/hgn/sosalert/SosAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hgn.sosalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosAlertServiceApplication.class, args);
+	}
+
+}
